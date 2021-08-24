468b40157faa9b2bd4980883f42451651d4787e0
Stuart Groom <stuartgroom567@gmail.com>
2021-08-24 13:42:03 +0100

    first commit

---
+++ projectsTab/JS.js
@@ -0,0 +1,28 @@
+﻿const btns = document.querySelectorAll(".modal-open");
+const close_btns = document.querySelectorAll(".modal-close");
+const overlay = document.getElementById("overlay");
+
+btns.forEach((btn) => {
+    const target = btn.dataset.target;
+    btn.addEventListener("click", () => {
+        document.querySelector(target).classList.add("active");
+        overlay.classList.add("active");
+    })
+})
+
+close_btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        document.querySelector(btn.dataset.target).classList.remove("active");
+        overlay.classList.remove("active");
+    })
+})
+
+window.onclick = (e) => {
+    if (e.target == overlay) {
+        const modals = document.querySelectorAll(".modal");
+        modals.forEach((modal) => modal.classList.remove("active"));
+        overlay.classList.remove("active");
+    }
+}
+
+
